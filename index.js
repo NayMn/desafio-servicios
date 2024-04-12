4ce26992493aec037783241ad1143e46c6968760
najira <najira_daniela@hotmail.com>
2024-04-12 16:01:48 -0400

    error 404

--- index.js
+++ index.js
@@ -2,21 +2,25 @@ import express from 'express';
 import { engine } from 'express-handlebars';
 
 import { services } from './data/services.data.js'
+import path, { dirname } from 'path'
+import { title } from 'process';
 
 const app = express();
 
-// public directory
-app.use(express.static('public'))
+const __dirname = import.meta.dirname;
 
-app.use('/css', express.static('node_modules/bootstrap/dist/css/'))
-app.use('/js', express.static('node_modules/bootstrap/dist/js/'))
-app.use('/js', express.static('node_modules/jquery/dist'))
+// public directory
+app.use(express.static(path.join(__dirname, '/public')))
+
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css/')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js/')))
+app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 
 
 // handlebars
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
-app.set('views', './views');
+app.set('views', path.join(__dirname, './views'));
 
 app.get('/', (req, res) => {
     res.render('home', { title: "home page 2.0" });
@@ -35,12 +39,19 @@ app.get('/services/:name', (req, res) => {
     // buscar con find en un array:
     const service = services.find((item) => item.url === `/services/${nameUrl}`)
 
-    console.log(service)
+    //   undefined
+
+    if (!service) {
+        return res.render('404', { title: "no se encuentra el servicio" })
+    }
+
     return res.render('service', { service })
 
+});
 
+app.get('*', (req, res) => {
+    return res.status(404).render('404', { title: 'no se encuentra la pagina' });
 })
-
 
 app.listen(5000, () => console.log('servidor encendido en http:/localhost:5000'))
 
