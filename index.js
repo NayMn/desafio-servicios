ada122d9a241c7f12a43152964fed37253ee0f1a
najira <najira_daniela@hotmail.com>
2024-04-12 15:06:53 -0400

    estructura con carpeta publica

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { engine } from 'express-handlebars';
 
+import { services } from './data/services.data.js'
+
 const app = express();
 
 // public directory
@@ -17,13 +19,27 @@ app.set('view engine', '.hbs');
 app.set('views', './views');
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', { title: "home page 2.0" });
 });
 
+
+// creacion de ruta
 app.get('/services', (req, res) => {
-    res.render('services');
+    res.render('services', { services: services });
 });
+// responder con informacion dinamica
+app.get('/services/:name', (req, res) => {
 
+    const nameUrl = req.params.name //qa
+
+    // buscar con find en un array:
+    const service = services.find((item) => item.url === `/services/${nameUrl}`)
+
+    console.log(service)
+    return res.render('service', { service })
+
+
+})
 
 
 app.listen(5000, () => console.log('servidor encendido en http:/localhost:5000'))
