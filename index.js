88d61f99134421ffc528030f4b9f51986357e88a
najira <najira_daniela@hotmail.com>
2024-04-12 12:09:28 -0400

    estructura de la pagina modularizada

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-import express from 'express'
-const app = express()
+import express from 'express';
+import { engine } from 'express-handlebars';
+
+const app = express();
+
+app.engine('.hbs', engine({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+app.set('views', './views');
 
 app.get('/', (req, res) => {
-    resizeBy.json({ ok: true })
-})
+    res.render('home');
+});
 
-app.listen(5000, () => console.log('estoy escuchando'))
+app.get('/services', (req, res) => {
+    res.render('services');
+});
+
+
+
+app.listen(5000, () => console.log('servidor encendido en http:/localhost:5000'))
 
 
 
